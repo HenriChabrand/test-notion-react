e44a4fd8346cd381e3999b983783a9a23190c41e
Henri <HenriChabrand@users.noreply.github.com>
2021-06-12 10:05:27 +0100

    Update [pageId].tsx

--- pages/[pageId].tsx
+++ pages/[pageId].tsx
@@ -47,7 +47,7 @@ export async function getStaticPaths() {
     }
   )
 
-  const paths = Object.keys(pages).map((pageId) => `/${pageId}-test-abc`)
+  const paths = Object.keys(pages).map((pageId, i) => '/path-'+i) // `/${pageId}-test-abc`
   console.log("paths",paths);
   
   return {
